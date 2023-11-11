9d5198aacc3dfefe8ea05d3414a7359225ea2ed6
LD <logan.d.m.117@gmail.com>
2023-11-10 21:34:49 -0500

    Game is working
    All win conditions have checks now, just trying to spruce up the way the game looks, clean up the code for readability, and then make an example AI to play against.
    
    FOR THE AI TOURNAMENT SYSTEM:
    
    Make it so that each script is added to a List in the first tournament scene and then randomly choose a tournament bracket and make the AI fight against each other. then do each layer in the bracket and get up to the top.
    
    USE A BINARY TREE FOR THAT.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Grid : MonoBehaviour
 {
+    private bool isRedTurn = true;
+
+    public bool won => didWin(); //won lambda makes it so that
+                                 //this variable is always set equal
+                                 //to the didWin() call output.
+
     //coin prefabs
     public GameObject redCoin;
     public GameObject yellowCoin;
+    public GameObject tile;
 
     //slots[x][y]
     List<List<Slot>> slots = new List<List<Slot>>(7) { new List<Slot>(5), new List<Slot>(5), new List<Slot>(5), new List<Slot>(5), new List<Slot>(5), new List<Slot>(5), new List<Slot>(5) };
@@ -36,6 +44,8 @@ public class Grid : MonoBehaviour
             {
                 y = lastY + 1.25f;
                 slots[i].Add(new Slot(i, j, x, y));
+                //make the background tile
+                GameObject.Instantiate(tile, new Vector2(x, y), Quaternion.identity);
                 lastY = y;
             }
             lastX = x;
@@ -51,11 +61,7 @@ public class Grid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            //TODO: 
-            //Instantiate a coin in this column and lerp it to the bottom of the column.
-        }
+
     }
 
     private void OnDrawGizmos()
@@ -64,38 +70,203 @@ public class Grid : MonoBehaviour
         {
             foreach (Slot s in ls)
             {
+                //tell the grid to draw the gizmo of itself.
                 s.drawGizmos();
             }
         }
     }
 
+    /// <summary>
+    /// Called everytime a player makes a move.
+    /// Used to reset data for bots by sending an event
+    /// Used to change game data and check win conditions.
+    /// </summary>
+    public void switchTurn()
+    {
+        //change turns.
+        isRedTurn = !isRedTurn;
+
+
+        for (int i = 0; i < slots.Count; i++)
+        {
+            //Vertical win check, HAVE NOT CHECKED FOR WHEN THEY START
+            //COINS OF A SPECIFIC COLOR HIGHER UP SWITCHING, CHECK FOR THAT.
+            if (!isColumnEmpty(i) && getColumnCoinCount(i) > 3)
+            {
+/*                List<Slot> fullSlots = getCoinsInColumn(i);
+                //Debug.Log("Column: " + i + " coins: " + getColumnCoinCount(i));
+                if (fullSlots != null)
+                {
+                    Debug.Log(fullSlots);
+                    if (fullSlots.TrueForAll(s => s.coin.CompareTag("Red")) || fullSlots.TrueForAll(s => s.coin.CompareTag("Yellow")))
+                    {
+                        Debug.Log("WIN");
+                    }
+                }*/
+            }
+
+            
+        }
+
+        if (didWin())
+        {
+            Debug.Log("WIN");
+        }
+       
+
+
+    }
+
     public void placeCoin(int column)
     {
-        if (!isColumnFull(column))
+        
+
+        if (!won && !isColumnFull(column))
         {
-            GameObject t = GameObject.Instantiate(redCoin, slots[column - 1].Find(s => s.isEmpty()).position, Quaternion.identity);
-            slots[column - 1].Find(s => s.isEmpty()).coin = t.GetComponent<Coin>();
+            //instatiate coin object at the location of the first empty slot.
+            GameObject t = GameObject.Instantiate(isRedTurn ? redCoin : yellowCoin, slots[column].Find(s => s.isEmpty()).position, Quaternion.identity);
+            //set the coin of the slot we just filled to be
+            //the coin component of the coin object we just
+            //created.
+            slots[column].Find(s => s.isEmpty()).coin = t.GetComponent<Coin>();
+
+            //Switch turns
+            switchTurn();
+        }
+        else
+        {
+            //TODO: play some audio and then also grey out the column for a moment.
         }
     }
 
     /// <summary>
     /// Returns true if the column is empty, false otherwise.
     /// </summary>
-    /// <param name="column">Column (1-7)</param>
+    /// <param name="column">Column (0-6)</param>
     /// <returns></returns>
     public bool isColumnEmpty(int column)
     {
-        return slots[column - 1].TrueForAll(s => s.isEmpty());
+        return slots[column].TrueForAll(s => s.isEmpty());
     }
 
     /// <summary>
     /// Returns true if the column is full, false otherwise.
     /// </summary>
-    /// <param name="column">Column (1-7)</param>
+    /// <param name="column">Column (0-6)</param>
     /// <returns></returns>
     public bool isColumnFull(int column)
     {
-        return slots[column - 1].TrueForAll(s => !s.isEmpty());
+        return slots[column].TrueForAll(s => !s.isEmpty());
+    }
+
+    public int getColumnCoinCount(int column)
+    {
+        if (isColumnEmpty(column))
+        {
+            return 0;
+        }
+        else if (isColumnFull(column))
+        {
+            return 7;
+        }
+        return slots[column].Where(s => !s.isEmpty()).Count();
+    }
+
+    /// <summary>
+    /// get a list of all the slots with a coin in the given column.
+    /// </summary>
+    /// <param name="column"></param>
+    /// <returns></returns>
+    public List<Slot> getCoinsInColumn(int column)
+    {
+        if (isColumnEmpty(column))
+        {
+            return null;
+        }
+        return slots[column].Where(s => !s.isEmpty())?.ToList();
+    }
+
+    public bool didWin()
+    {
+
+        //Vertical Check
+        for (int i = 0; i < slots.Count; i++)
+        {
+            for (int j = 0; j < slots[i].Count - 3; j++)
+            {
+                if (!slots[i][j].isEmpty() && !slots[i][j + 1].isEmpty() && !slots[i][j + 2].isEmpty() && !slots[i][j + 3].isEmpty())
+                {
+                    if (slots[i][j].coin.CompareTag(slots[i][j + 1].coin.tag) && slots[i][j + 1].coin.CompareTag(slots[i][j + 2].coin.tag) && slots[i][j + 2].coin.CompareTag(slots[i][j + 3].coin.tag))
+                    {
+                        slots[i][j].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i][j + 1].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i][j + 2].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i][j + 3].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        //horizontal check
+        for (int i = 0; i < slots.Count-3; i++)
+        {
+            for (int j = 0; j < slots[i].Count; j++)
+            {
+                if (!slots[i][j].isEmpty() && !slots[i + 1][j].isEmpty() && !slots[i+2][j].isEmpty() && !slots[i+3][j].isEmpty())
+                {
+                    if (slots[i][j].coin.CompareTag(slots[i+1][j].coin.tag) && slots[i+1][j].coin.CompareTag(slots[i+2][j].coin.tag) && slots[i+2][j].coin.CompareTag(slots[i+3][j].coin.tag))
+                    {
+                        slots[i][j].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i+1][j].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i+2][j].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i+3][j].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        //Right diagonal check
+        for (int i = 3; i < slots.Count; i++)
+        {
+            for (int j = 0; j < slots[i].Count - 3; j++)
+            {
+                if (!slots[i][j].isEmpty() && !slots[i - 1][j + 1].isEmpty() && !slots[i - 2][j + 2].isEmpty() && !slots[i - 3][j + 3].isEmpty())
+                {
+                    if (slots[i][j].coin.CompareTag(slots[i - 1][j + 1].coin.tag) && slots[i - 1][j + 1].coin.CompareTag(slots[i - 2][j + 2].coin.tag) && slots[i - 2][j + 2].coin.CompareTag(slots[i - 3][j + 3].coin.tag))
+                    {
+                        slots[i][j].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i - 1][j + 1].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i - 2][j + 2].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i - 3][j + 3].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        //Left diagonal check
+        for (int i = 3; i < slots.Count; i++)
+        {
+            for (int j = 3; j < slots[i].Count; j++)
+            {
+                if (!slots[i][j].isEmpty() && !slots[i - 1][j-1].isEmpty() && !slots[i - 2][j-2].isEmpty() && !slots[i - 3][j-3].isEmpty())
+                {
+                    if (slots[i][j].coin.CompareTag(slots[i - 1][j-1].coin.tag) && slots[i - 1][j-1].coin.CompareTag(slots[i - 2][j-2].coin.tag) && slots[i - 2][j-2].coin.CompareTag(slots[i - 3][j-3].coin.tag))
+                    {
+                        slots[i][j].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i - 1][j-1].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i - 2][j-2].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        slots[i - 3][j-3].coin.GetComponent<SpriteRenderer>().color = Color.green;
+                        return true;
+                    }
+                }
+            }
+        }
+
+
+        return false;
     }
 }
 
@@ -153,7 +324,7 @@ public class Slot
 
     public void drawGizmos()
     {
-        Gizmos.color = Color.black;
+        Gizmos.color = this.isEmpty() ? Color.black : Color.green;
         Gizmos.DrawWireSphere(position, 0.5f);
     }
 }
